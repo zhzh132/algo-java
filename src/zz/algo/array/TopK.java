80f89e0d970fa857c022a2128b59c45a6ffda989
qqmm <qqmm@qqmm-PC.lan>
2017-02-04 18:22:38 +0800

    top k using heap

--- src/zz/algo/array/TopK.java
+++ src/zz/algo/array/TopK.java
@@ -45,4 +45,44 @@ public class TopK {
 		} 
 	}
 	
+	public static int[] top_K_heap(int[] arr, int k) {
+		int[] result = new int[k];
+		for(int i = 0; i < k; i++) {
+			result[i] = arr[i];
+		}
+		buildMinHeap(result);
+		for(int i = k; i < arr.length; i++) {
+			if(arr[i] > result[0]) {
+				result[0] = arr[i];
+				minHeapify(result, 0);
+			}
+		}
+		Arrays.sort(result);
+		return result;
+	}
+	
+	private static void buildMinHeap(int[] a) {
+		for(int i = a.length / 2 - 1; i >= 0; i--) {
+			minHeapify(a, i);
+		}
+	}
+	
+	private static void minHeapify(int[] a, int i) {
+		int l = i * 2 + 1;
+		int r = i * 2 + 2;
+		int smallest;
+		if(l < a.length && a[l] <= a[i]) {
+			smallest = l;
+		}
+		else {
+			smallest = i;
+		}
+		if(r < a.length && a[r] <= a[smallest]) {
+			smallest = r;
+		}
+		if(smallest != i) {
+			swap(a, smallest, i);
+			minHeapify(a, smallest);
+		}
+	}
 }
--- test/zz/algo/array/TopKTest.java
+++ test/zz/algo/array/TopKTest.java
@@ -19,4 +19,18 @@ public class TopKTest {
 		int[] ret = TopK.top_K(arr, 6);
 		assertArrayEquals(new int[]{1,2,3,4,5,6}, ret);
 	}
+	
+	@Test
+	public void test3() {
+		int[] arr = {1,5,3,6,8,2,4,7,9};
+		int[] ret = TopK.top_K_heap(arr, 3);
+		assertArrayEquals(new int[]{7,8,9}, ret);
+	}
+
+	@Test
+	public void test4() {
+		int[] arr = {1,5,3,6,8,2,4,7,9};
+		int[] ret = TopK.top_K_heap(arr, 6);
+		assertArrayEquals(new int[]{4,5,6,7,8,9}, ret);
+	}
 }
